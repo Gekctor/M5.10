e631412dc6c87111f7d56dd972f55a5cf9fe22b9
Gekctor <93215238+Gekctor@users.noreply.github.com>
2023-03-22 19:30:17 +0600

    Init 5.10
    Initial

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int num, int den) : numerator(num), denominator(den) {
+        if (denominator == 0) {
+            throw std::runtime_error("Denominator cannot be zero");
+        }
+    }
+
+    void print() {
+        std::cout << numerator << "/" << denominator << std::endl;
+    }
+};
+
+int main() {
+    try {
+        int num, den;
+        std::cout << "Enter numerator: ";
+        std::cin >> num;
+        std::cout << "Enter denominator: ";
+        std::cin >> den;
+
+        Fraction f(num, den);
+        f.print();
+    }
+    catch (std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
